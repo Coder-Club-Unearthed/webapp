eb3081cc2d66e61e3854671d337368efdc63a4d2
rfern <randallfernando@yahoo.com.au>
2015-05-17 12:29:27 +1000

    changing default centering of map

--- proj/src/static/site/js/geostockpiles.js
+++ proj/src/static/site/js/geostockpiles.js
@@ -1,5 +1,5 @@
 function addMap() {
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    var map = L.map('map').setView([-21.79659234, 147.94955492], 13);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18,
